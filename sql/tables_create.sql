b603064b2b95ea9fff5897763af57739d59369f1
Mohamed Saidi <saidimohamedali@hotmail.fr>
2016-03-16 10:23:12 +0100

    new sql file + template ajust

--- sql/tables_create.sql
+++ sql/tables_create.sql
@@ -1,18 +1,233 @@
-create table code_classe(code varchar(20) not null, nom varchar(20) not null, primary key (code));
-create table annee_scolaire(code varchar(20) not null, designation varchar(20) not null, primary key (code));
-create table matiere(code varchar(20) not null, designation varchar(20) not null, primary key(code));
-create table eleve(nom varchar(20) not null, prenom varchar(20) not null, matricule varchar(40) not null, adresse varchar(100) not null, date_naissance date not null, primary key (matricule));
-create table moyenne(matricule varchar(40) not null, annee_scolaire varchar(20) not null, trimestre int not null, moyenne double not null, primary key (matricule, annee_scolaire, trimestre));
-create table note(code_matiere varchar(20) not null, note double not null, matricule varchar(40) not null, annee_scolaire varchar(20) not null, trimestre int not null, primary key (code_matiere, matricule, annee_scolaire, trimestre));
-create table presence (matricule varchar(40) not null, valeur int not null, date date not null, seance int not null, primary key (matricule,date,seance));
-create table classe(matricule varchar(40) not null, annee_scolaire varchar(20) not null, code_classe varchar(20) not null, primary key (matricule, annee_scolaire, code_classe));
+-- MySQL dump 10.15  Distrib 10.0.23-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: ecole
+-- ------------------------------------------------------
+-- Server version	10.0.23-MariaDB-0ubuntu0.15.10.1
 
-alter table classe add constraint fk_classe_matricule foreign key (matricule) references eleve(matricule);
-alter table classe add constraint fk_classe_annee foreign key (annee_scolaire) references annee_scolaire(code);
-alter table classe add constraint fk_classe_code_classe foreign key (code_classe) references code_classe(code);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-alter table note add constraint fk_note_matiere foreign key (code_matiere) references matiere(code);
-alter table note add constraint fk_note_matricule foreign key (matricule) references eleve(matricule);
-alter table note add constraint fk_note_annee foreign key (annee_scolaire) references annee_scolaire(code);
+--
+-- Table structure for table `annee_scolaire`
+--
 
-alter table presence add constraint fk_precense_matricule foreign key (matricule) references eleve(matricule);
+DROP TABLE IF EXISTS `annee_scolaire`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `annee_scolaire` (
+  `code` varchar(20) NOT NULL,
+  `designation` varchar(20) NOT NULL,
+  PRIMARY KEY (`code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `annee_scolaire`
+--
+
+LOCK TABLES `annee_scolaire` WRITE;
+/*!40000 ALTER TABLE `annee_scolaire` DISABLE KEYS */;
+/*!40000 ALTER TABLE `annee_scolaire` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `classe`
+--
+
+DROP TABLE IF EXISTS `classe`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `classe` (
+  `matricule` varchar(40) NOT NULL,
+  `annee_scolaire` varchar(20) NOT NULL,
+  `code_classe` varchar(20) NOT NULL,
+  PRIMARY KEY (`matricule`,`annee_scolaire`,`code_classe`),
+  KEY `fk_classe_annee` (`annee_scolaire`),
+  KEY `fk_classe_code_classe` (`code_classe`),
+  CONSTRAINT `fk_classe_annee` FOREIGN KEY (`annee_scolaire`) REFERENCES `annee_scolaire` (`code`),
+  CONSTRAINT `fk_classe_code_classe` FOREIGN KEY (`code_classe`) REFERENCES `code_classe` (`code`),
+  CONSTRAINT `fk_classe_matricule` FOREIGN KEY (`matricule`) REFERENCES `eleve` (`matricule`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `classe`
+--
+
+LOCK TABLES `classe` WRITE;
+/*!40000 ALTER TABLE `classe` DISABLE KEYS */;
+/*!40000 ALTER TABLE `classe` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `code_classe`
+--
+
+DROP TABLE IF EXISTS `code_classe`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `code_classe` (
+  `code` varchar(20) NOT NULL,
+  `nom` varchar(20) NOT NULL,
+  PRIMARY KEY (`code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `code_classe`
+--
+
+LOCK TABLES `code_classe` WRITE;
+/*!40000 ALTER TABLE `code_classe` DISABLE KEYS */;
+/*!40000 ALTER TABLE `code_classe` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `eleve`
+--
+
+DROP TABLE IF EXISTS `eleve`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `eleve` (
+  `nom` varchar(20) NOT NULL,
+  `prenom` varchar(20) NOT NULL,
+  `matricule` varchar(40) NOT NULL,
+  `adresse` varchar(100) NOT NULL,
+  `date_naissance` date NOT NULL,
+  PRIMARY KEY (`matricule`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `eleve`
+--
+
+LOCK TABLES `eleve` WRITE;
+/*!40000 ALTER TABLE `eleve` DISABLE KEYS */;
+/*!40000 ALTER TABLE `eleve` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `matiere`
+--
+
+DROP TABLE IF EXISTS `matiere`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `matiere` (
+  `code` varchar(20) NOT NULL,
+  `designation` varchar(20) NOT NULL,
+  PRIMARY KEY (`code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `matiere`
+--
+
+LOCK TABLES `matiere` WRITE;
+/*!40000 ALTER TABLE `matiere` DISABLE KEYS */;
+/*!40000 ALTER TABLE `matiere` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `moyenne`
+--
+
+DROP TABLE IF EXISTS `moyenne`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `moyenne` (
+  `matricule` varchar(40) NOT NULL,
+  `annee_scolaire` varchar(20) NOT NULL,
+  `trimestre` int(11) NOT NULL,
+  `moyenne` double NOT NULL,
+  PRIMARY KEY (`matricule`,`annee_scolaire`,`trimestre`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `moyenne`
+--
+
+LOCK TABLES `moyenne` WRITE;
+/*!40000 ALTER TABLE `moyenne` DISABLE KEYS */;
+/*!40000 ALTER TABLE `moyenne` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `note`
+--
+
+DROP TABLE IF EXISTS `note`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `note` (
+  `code_matiere` varchar(20) NOT NULL,
+  `note` double NOT NULL,
+  `matricule` varchar(40) NOT NULL,
+  `annee_scolaire` varchar(20) NOT NULL,
+  `trimestre` int(11) NOT NULL,
+  PRIMARY KEY (`code_matiere`,`matricule`,`annee_scolaire`,`trimestre`),
+  KEY `fk_note_matricule` (`matricule`),
+  KEY `fk_note_annee` (`annee_scolaire`),
+  CONSTRAINT `fk_note_annee` FOREIGN KEY (`annee_scolaire`) REFERENCES `annee_scolaire` (`code`),
+  CONSTRAINT `fk_note_matiere` FOREIGN KEY (`code_matiere`) REFERENCES `matiere` (`code`),
+  CONSTRAINT `fk_note_matricule` FOREIGN KEY (`matricule`) REFERENCES `eleve` (`matricule`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `note`
+--
+
+LOCK TABLES `note` WRITE;
+/*!40000 ALTER TABLE `note` DISABLE KEYS */;
+/*!40000 ALTER TABLE `note` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `presence`
+--
+
+DROP TABLE IF EXISTS `presence`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `presence` (
+  `matricule` varchar(40) NOT NULL,
+  `valeur` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `seance` int(11) NOT NULL,
+  PRIMARY KEY (`matricule`,`date`,`seance`),
+  CONSTRAINT `fk_precense_matricule` FOREIGN KEY (`matricule`) REFERENCES `eleve` (`matricule`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `presence`
+--
+
+LOCK TABLES `presence` WRITE;
+/*!40000 ALTER TABLE `presence` DISABLE KEYS */;
+/*!40000 ALTER TABLE `presence` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-03-16 10:22:05
